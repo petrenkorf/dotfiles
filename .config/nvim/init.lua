df1b02f235d419215c5ddbaf6a07afc1a26cea1e
Petris Fernandes <petrisrf@gmail.com>
2025-10-11 10:42:06 -0300

    Show line numbers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,8 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.opt.wrap = false
 vim.cmd("set number")
+vim.opt.wrap = false
 -- vim.cmd("set autoindent")
 -- vim.cmd("set smartindent")
 -- vim.cmd("filetype plugin indent on")
