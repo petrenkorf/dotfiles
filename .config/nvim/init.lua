9a39bf1cde402fe37514895473e144c96264a7ac
Petris Fernandes <petrisrf@gmail.com>
2025-12-19 05:25:41 -0300

    Fixing Autocomplete

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -131,7 +131,7 @@ local plugins = {
         },
         indent = {
           enable = true,
-          -- disable = { "ruby" }
+          disable = { "ruby" }
         },
         autotag = {
           enabled = true
@@ -344,7 +344,7 @@ local plugins = {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          -- ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
