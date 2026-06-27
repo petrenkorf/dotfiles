1a5faafa076f40793c52f629fb65fd81c796a91c
Petris <petrisrf@gmail.com>
2026-06-27 18:01:26 -0300

    Execute whole spec file based on last run spec or current file

--- .config/nvim/lua/plugins/testing.lua
+++ .config/nvim/lua/plugins/testing.lua
@@ -29,9 +29,15 @@ return {
       local rspec_term = Terminal:new({
         direction = "float",
         hidden = true,
+        on_open = function(term)
+          vim.keymap.set("t", "<Esc>", function()
+            term:close()
+          end, { buffer = term.bufnr, noremap = true, silent = true })
+        end,
       })
 
       local last_rspec_cmd = nil
+      local last_rspec_file = nil
 
       function RunRSpecCurrentLine()
         local file = vim.fn.expand("%")
@@ -46,6 +52,7 @@ return {
         if file:match("_spec%.rb$") then
           cmd = string.format("bundle exec rspec %s:%d", file, line)
           last_rspec_cmd = cmd
+          last_rspec_file = file
         elseif last_rspec_cmd then
           cmd = last_rspec_cmd
         else
@@ -53,12 +60,33 @@ return {
           return
         end
 
-        rspec_term:toggle()
+        rspec_term:open()
         rspec_term:send(cmd)
+      end
+
+      function RunRSpecFile()
+        local file = vim.fn.expand("%")
+
+        local target
+        if file:match("_spec%.rb$") then
+          target = file
+          last_rspec_file = file
+        elseif last_rspec_file then
+          target = last_rspec_file
+        else
+          print("No spec has been run yet")
+          return
+        end
+
+        rspec_term:open()
+        rspec_term:send(string.format("bundle exec rspec %s", target))
       end
 
       vim.keymap.set("n", "<leader>tt", function()
         RunRSpecCurrentLine()
+      end)
+      vim.keymap.set("n", "<leader>ta", function()
+        RunRSpecFile()
       end)
       vim.keymap.set("n", "<leader>tf", function() neotest.run.run(vim.fn.expand("%")) end,
         { desc = "Run current file" })
