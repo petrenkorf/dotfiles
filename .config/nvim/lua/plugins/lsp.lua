a54fcf7e999239b90044380ee57ac6443f27509b
Petris <petrisrf@gmail.com>
2026-06-27 17:31:44 -0300

    Fix cursor position when identing after saving

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -72,7 +72,9 @@ return {
       vim.api.nvim_create_autocmd("BufWritePre", {
         pattern = { "*.rb" },
         callback = function()
+          local pos = vim.api.nvim_win_get_cursor(0)
           vim.cmd("normal! gg=G")
+          vim.api.nvim_win_set_cursor(0, pos)
         end,
       })
     end
